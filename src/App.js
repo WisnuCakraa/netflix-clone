7b3f37b8746f8e319f6c9355961dcd5db40d73fc
wisnucakraa <wisnucakraa@gmail.com>
2020-08-05 15:31:37 +0700

    update firebase hosting

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Nav />
      <Banner fetchUrl={requests.fetchNetflixOriginals}/>
      <Row 
-        title='NETFLIX ORIGINALSs' 
+        title='NETFLIX ORIGINALS' 
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow={true}
         />
